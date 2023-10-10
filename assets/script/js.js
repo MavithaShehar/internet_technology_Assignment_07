cb052a0b2d6894a7ca59c7129e7273af8c2bdbf2
MavithaShehar <mavithashehar@gmail.com>
2023-10-11 00:56:53 +0530

    apply jQuerry for create to SPA

---
+++ assets/script/js.js
@@ -0,0 +1,35 @@
+$('#nav-dashboard').on('click',()=>{
+
+    $('#dashboard-section').css('display', 'block');
+    $('#customer-section').css('display', 'none');
+    $('#item-section').css('display', 'none');
+    $('#order-section').css('display', 'none');
+
+})
+
+$('#nav-customer').on('click',()=>{
+
+    $('#dashboard-section').css('display', 'none');
+    $('#customer-section').css('display', 'block');
+    $('#item-section').css('display', 'none');
+    $('#order-section').css('display', 'none');
+
+})
+
+$('#nav-item').on('click',()=>{
+
+    $('#dashboard-section').css('display', 'none');
+    $('#customer-section').css('display', 'none');
+    $('#item-section').css('display', 'block');
+    $('#order-section').css('display', 'none');
+
+})
+
+$('#nav-orders').on('click',()=>{
+
+    $('#dashboard-section').css('display', 'none');
+    $('#customer-section').css('display', 'none');
+    $('#item-section').css('display', 'none');
+    $('#order-section').css('display', 'block');
+
+})
